f63e01df4ce250c12f187251086811d0ef3bb953
Xi LIU <xi.liu@u.nus.edu>
2024-07-22 19:57:53 +0800

    boostmc

--- boost_integral_mc/gen_mc.hpp
+++ boost_integral_mc/gen_mc.hpp
@@ -23,34 +23,95 @@
 #include <vector>
 
 namespace fs = boost::filesystem;
+const auto PI=M_PI;
 
 class gen_data{
 
-public:gen_data(){
+public:gen_data() {
 
-        this->a=1;
-         this->b=2;
-        this->sgm=0.1;
-        this->M=
-}
+        this->a = 1.0;
+        this->b = 2.0;
+        this->sgm = 0.1;
+        this->xInit=a+(b-a)/1.76;
+
+        this->M = std::pow(2.0 * PI, 0.5) * sgm * 1.001;
+        dataDir = "./boost_int_mc/";
+        if (!fs::is_directory(dataDir) || !fs::exists(dataDir)) {
+            fs::create_directories(dataDir);
+        }
+        loopToWrite = 1000000;
+        newFlushNum = 5;
+
+        this->dataAll = std::shared_ptr<double[]>(new double[loopToWrite],
+                                                  std::default_delete<double[]>());
+
+    }
 
 public:
-///proportional to probability
-double f(const double &x);
+///proportional to probability, target mc distribution
+double g(const double &x);
+
 
 ///
 /// @param y
-/// @param x
-/// @return known proposal function
+/// @param x center
+/// @return known proposal function, which is normal distribution
 double Q(const double &y, const double &x);
 
+
+///
+/// @param x center
+/// @return random number from truncated Gaussian
+double reject_sampling_one_data(const double &x);
+
+///
+/// @param y
+/// @param x center
+/// @return truncated Gaussian
+double f(const double &y, const double& x);
+
+///
+/// @param xCurr
+/// @param xNext
+/// @return
+void proposal(const double &xCurr, double &xNext);
+
+///
+/// @param x center
+/// @return integral
+double zVal(const double& x);
+
+///
+/// @param xCurr
+/// @param xNext
+/// @return
+double  acceptanceRatio(const double &xCurr, const double &xNext);
+
+
+///
+/// @param y
+/// @param x center
+/// @return
+double integrand(const double &y, const double& x);
+
+void execute_mc(const double& x);
+
+
+void saveArrayToCSV(const std::shared_ptr<double[]>& array, const  size_t& arraySize, const std::string& filename);
+
+    void init_and_run();
 
 public:
     double a;
     double b;
     double sgm;
+    double xInit;
 
     double M;
+    std::string dataDir;
+    std::shared_ptr<double[]> dataAll;
+    size_t loopToWrite;
+    size_t newFlushNum;
 
 };
 
--- boost_integral_mc/loadBoostMc.py
+++ boost_integral_mc/loadBoostMc.py
@@ -6,21 +6,24 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from scipy.stats import ks_2samp
 import json
-#This script loads raw data and take effective data
-inPath="./testS/"
-flushAll=[]
+
+#This script loads raw data  of mc using boost integral and take effective data
+
+
+
+inPath="../boost_int_mc/"
+loopEndAll=[]
 inCsvFilesAll=[]
+
 for file in glob.glob(inPath+"/*.csv"):
-    matchFlsh=re.search(r"flush(\d+)",file)
-    if matchFlsh:
-        flushAll.append(int(matchFlsh.group(1)))
+    matchLoopEnd=re.search(r"loopEnd(\d+)",file)
+    if matchLoopEnd:
+        loopEndAll.append(int(matchLoopEnd.group(1)))
         inCsvFilesAll.append(file)
 
-
-sortedInds=np.argsort(flushAll)
-
+sortedInds=np.argsort(loopEndAll)
 sortedCsvFiles=[inCsvFilesAll[ind] for ind in sortedInds]
-# print(sortedCsvFiles)
+
 xCombined=np.array([])
 
 # x=pd.read_csv(sortedCsvFiles[0])
@@ -31,11 +34,6 @@ for file in sortedCsvFiles:
     xVecNp=np.array(xVecTmp.iloc[:,0])
     xCombined=np.r_[xCombined,xVecNp]
 
-# plt.figure()
-# plt.scatter(xCombined,color="black")
-# plt.savefig("tmp.png")
-# plt.close()
-
 
 xEqPosition=int(len(xCombined)*4/11)
 
@@ -43,6 +41,7 @@ xTruncated=xCombined[xEqPosition:]
 
 corr_eps=1e-3
 NLags=int(len(xTruncated*6/7))
+
 
 acf_xTruncated=sm.tsa.acf(xTruncated,nlags=NLags)
 acf_xTruncatedAbs=np.abs(acf_xTruncated)
@@ -80,5 +79,3 @@ if min_acf_x<corr_eps:
     jsonOut={"xEff":list(xVecValsToCompute)}
     with open(outFile,"w+")as fptr:
         json.dump(jsonOut,fptr,indent=4)
-
-
