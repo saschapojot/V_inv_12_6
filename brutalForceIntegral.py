ae6ac5d79f4e6baa5d7712ded2a97c2ab4bfe6ef
Xi LIU <xi.liu@u.nus.edu>
2024-07-27 20:33:02 +0800

    br

--- brutalForceIntegral.py
+++ brutalForceIntegral.py
@@ -1,8 +1,6 @@
 import numpy as np
 import scipy.integrate as integrate
 # import pandas as pd
-import vegas
-
 #this script benchmarks the mc computation by brutal force integration
 
 rowNum=0
@@ -15,12 +13,14 @@ b1=80
 a2=15
 b2=67
 
+shift=265
 
-def V(L,y0,z0,y1):
+def V(L, y0, z0, y1):
+    # L, y0, z0, y1 = x
     val=a1*y0**(-12)-b1*y0**(-6)\
         +a2*z0**(-12)-b2*z0**(-6)\
         + a1*y1**(-12)-b1*y1**(-6)\
-        + a2*(-y0-z0-y1+L)**(-12)-b2*(-y0-z0-y1+L)**(-6)
+        + a2*(-y0-z0-y1+L)**(-12)-b2*(-y0-z0-y1+L)**(-6)+shift
 
 
     return val
@@ -29,7 +29,7 @@ def V(L,y0,z0,y1):
 r1=(2*a1/b1)**(1/6)
 r2=(2*a2/b2)**(1/6)
 
-eps=(r1+r1)/50
+eps=(r1+r1)/30
 
 y0Range=[r1-eps,r1+eps]
 
@@ -39,20 +39,37 @@ y1Range=[r1-eps,r1+eps]
 
 LRange=[2*r1+2*r2-eps,2*r1+2*r2+eps]
 
-def f(x, beta):
-    L, y0, z0, y1 = x
+def Z(L, y0, z0, y1, beta):
+    #
     return np.exp(-beta * V(L, y0, z0, y1))
 
+T=0.5
+beta = 1/T
+result, error = integrate.nquad(lambda L, y0, z0, y1: Z(L, y0, z0, y1, beta), [LRange, y0Range, z0Range, y1Range])
 
-beta = 1.0
+print("Integral result:", result)
+print("Estimated error:", error)
 
-limits =[LRange,y0Range,z0Range,y1Range]
-# Define the integrand with the beta parameter
-integrand = lambda x: f(x, beta)
 
-# Perform the 4-dimensional integral using vegas
-integ = vegas.Integrator(limits)
-result = integ(integrand, nitn=100, neval=100000)
+rstV,errV=integrate.nquad(lambda  L, y0, z0, y1: V(L, y0, z0, y1)*Z(L, y0, z0, y1, beta), [LRange, y0Range, z0Range, y1Range])
 
-print("Integral result:", result.mean)
-print("Estimated error:", result.sdev)
+EV=rstV/result-shift
+print(EV)
+# limits =[LRange,y0Range,z0Range,y1Range]
+# # Define the integrand with the beta parameter
+# integrandZ = lambda x: Z(x, beta)
+#
+# # Perform the 4-dimensional integral using vegas
+# integ = vegas.Integrator(limits)
+# result = integ(integrandZ, nitn=100, neval=100000)
+
+# print("Integral result:", result.mean)
+# print("Estimated error:", result.sdev)
+
+# integrandV=lambda x: V(x)*Z(x,beta)
+#
+# rstV=integ(integrandV,nitn=100, neval=100000)
+#
+# EV=rstV.mean/result.mean-shift
+#
+# print(EV)
