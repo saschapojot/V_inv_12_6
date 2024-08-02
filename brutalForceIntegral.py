9dfbee54736ef4093758218221716af42f977822
Xi LIU <xi.liu@u.nus.edu>
2024-08-02 18:58:18 +0800

    local

--- brutalForceIntegral.py
+++ brutalForceIntegral.py
@@ -2,8 +2,9 @@ import numpy as np
 import scipy.integrate as integrate
 import pandas as pd
 from datetime import datetime
+import json
 #this script benchmarks the mc computation by brutal force integration
-
+#This script computes Z
 rowNum=0
 inParamFileName="./V_inv_12_6Params.csv"
 
@@ -47,34 +48,43 @@ def Z(L, y0, z0, y1, beta):
 
 T=1
 beta = 1/T
-result, error = integrate.nquad(lambda L, y0, z0, y1: Z(L, y0, z0, y1, beta), [LRange, y0Range, z0Range, y1Range])
-
-print("Integral result:", result)
-print("Estimated error:", error)
-
-
-rstV,errV=integrate.nquad(lambda  L, y0, z0, y1: V(L, y0, z0, y1)*Z(L, y0, z0, y1, beta), [LRange, y0Range, z0Range, y1Range])
-
-print("Integral result of V:", rstV)
-print("Estimated error of EV:", errV)
-
-EV=rstV/result-shift
-print(EV)
-# limits =[LRange,y0Range,z0Range,y1Range]
-# # Define the integrand with the beta parameter
-# integrandZ = lambda x: Z(x, beta)
+# result, error = integrate.nquad(lambda L, y0, z0, y1: Z(L, y0, z0, y1, beta), [LRange, y0Range, z0Range, y1Range])
 #
-# # Perform the 4-dimensional integral using vegas
-# integ = vegas.Integrator(limits)
-# result = integ(integrandZ, nitn=100, neval=100000)
+# print("Integral result:", result)
+# print("Estimated error:", error)
 
-# print("Integral result:", result.mean)
-# print("Estimated error:", result.sdev)
 
-# integrandV=lambda x: V(x)*Z(x,beta)
+# rstV,errV=integrate.nquad(lambda  L, y0, z0, y1: V(L, y0, z0, y1)*Z(L, y0, z0, y1, beta), [LRange, y0Range, z0Range, y1Range])
 #
-# rstV=integ(integrandV,nitn=100, neval=100000)
+# print("Integral result of V:", rstV)
+# print("Estimated error of EV:", errV)
 #
-# EV=rstV.mean/result.mean-shift
-#
+# EV=rstV/result-shift
 # print(EV)
+def ZIntVal(T):
+    beta = 1/T
+    result, error = integrate.nquad(lambda L, y0, z0, y1: Z(L, y0, z0, y1, beta), [LRange, y0Range, z0Range, y1Range])
+    return [result,error]
+
+
+TVals=[0.1,0.3]
+ZVals=[]
+errVals=[]
+tIntStart=datetime.now()
+for T in TVals:
+    rst,err=ZIntVal(T)
+    ZVals.append(rst)
+    errVals.append(err)
+
+
+tIntEnd=datetime.now()
+
+outData={"T":list(TVals),
+         "Z":list(ZVals),
+         "err":list(errVals),
+         "shift":str(shift)}
+
+fileName="T"+str(TVals[0])+"_"+str(TVals[-1])+"Z.json"
+
+with open(fileName,"w+") as fptr:
+    json.dump(outData,fptr,indent=4)
